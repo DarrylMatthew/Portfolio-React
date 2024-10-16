e3510f2d5415779ae35eeff6c7d2171a4a796ac1
Darryl Matthew Wibawa <80321411+DarrylMatthew@users.noreply.github.com>
2024-10-16 10:40:17 +0700

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@ import './Navbar.css'
 export const Navbar = () => {
   return (
     <div className='nav'>
-        <div className="nav-logo">EV-olution</div>
+        <div className="nav-logo"><a href="#">EV-olution</a></div>
         <ul className='nav-menu'>
-            <li>Home</li>
-            <li>Explore</li>
-            <li>About</li>
-            <li className='nav-contact'>Contact</li>
+            <li><a href="https://www.byd.com/id/car/seal">Models</a></li>
+            <li><a href="https://www.byd.com/us/car/han-ev">Explore</a></li>
+            <li><a href="https://www.byd.com/us/about-byd">About</a></li>
+            <li className='nav-contact'><a href="https://www.byd.com/us/contact-us">Contact</a></li>
         </ul>
     </div>
   )
